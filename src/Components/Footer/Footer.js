cd5e19b740a9502eccbb863094da2621fbddf03b
Rahul Singh <rkant225@gmail.com>
2021-05-13 22:58:58 +0530

    changed style

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Footer = (props) => {
     return (
-        <div>
-            <div style={{position : 'absolute', bottom : 0, right : 10, fontSize : '.9rem', border : '1px solid gray', marginBottom : '.5rem', padding : '.5rem', boxShadow : '1px 4px gray'}}>
+            <div style={{position : 'fixed', bottom : 0, right : 10, fontSize : '.9rem', border : '1px solid gray', marginBottom : '.5rem', padding : '.5rem', boxShadow : '1px 4px gray'}}>
                 Developer @rkant225 (GitHub)
             </div>
-        </div>
     )
 }
 
